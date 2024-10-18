4c07c64a1f9b744c0a3f5c2efcaf32b65adac22d
Raghvendrasingh17 <Raghvendrasingh4698@gmail.com>
2024-10-19 03:02:26 +0530

    DRAMA-11

---
+++ Drama11-Awareness-main/public/script.js
@@ -0,0 +1,12 @@
+// Get the popup element
+var popup = document.getElementById('popup');
+
+// Display the popup when the page loads
+window.onload = function() {
+    popup.style.display = 'block';
+}
+
+// Function to close the popup
+function closePopup() {
+    popup.style.display = 'none';
+}
